ec6607120c466f0274945f2d8777981c2d0049bc
byesolarcat <patachyon@gmail.com>
2021-01-28 10:58:00 +0300

    Fix namings in "HorizontalLine"

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -6,11 +6,11 @@ namespace Snake
 	{
 		public HorizontalLine(int xLeft, int xRight, int y, char symbol)
 		{
-			pointsList = new List<Point>();
+			PointsList = new List<Point>();
 			for (int x = xLeft; x <= xRight; x++)
 			{
 				Point p = new Point(x, y, symbol);
-				pointsList.Add(p);
+				PointsList.Add(p);
 			}
 		}
 	}
