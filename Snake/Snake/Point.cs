1c61e8e55272dd0786d87dcbec6988b4d277f634
byesolarcat <patachyon@gmail.com>
2021-01-28 10:13:52 +0300

    Add "Point" class

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+	class Point
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public char Symbol { get; set; }
+
+		public void Draw()
+		{
+			Console.SetCursorPosition(this.X, this.Y);
+			Console.Write(this.Symbol);
+		}
+	}
+}
