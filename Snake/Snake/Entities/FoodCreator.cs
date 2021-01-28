d643471f563546963b2874c5fc5d38bc7d887c31
byesolarcat <patachyon@gmail.com>
2021-01-28 14:40:05 +0300

    Incapsulate food counter into "FoodCreator" class

--- Snake/Snake/Entities/FoodCreator.cs
+++ Snake/Snake/Entities/FoodCreator.cs
@@ -1,22 +1,37 @@
 ﻿using System;
+using System.Threading;
 
 namespace Snake
 {
 	class FoodCreator
 	{
 		private char symbol;
+		private int foodEaten;
 
 		private Random random = new Random();
+
 		public FoodCreator(char sym)
 		{
 			this.symbol = sym;
+			foodEaten = -1;
+
+			Console.SetCursorPosition(2, 1);
+			Console.Write("Food eaten: ");
 		}
 
 		public Point CreateFood()
 		{
+			UpdateFoodCounter();
 			int x = random.Next(2, Console.WindowWidth - 2);
 			int y = random.Next(4, Console.WindowHeight - 2);
 			return new Point(x, y, this.symbol);
+		}
+
+		private void UpdateFoodCounter()
+		{
+			foodEaten++;
+			Console.SetCursorPosition(14, 1);
+			Console.Write(foodEaten);
 		}
 	}
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -20,69 +20,61 @@ namespace Snake
 		static void StartGame()
 		{
 			Walls walls = new Walls();
-			walls.Draw();
-
-			Console.SetCursorPosition(2, 1);
-			Console.Write("Food eaten: 0");
-			int foodEaten = 0;
-
 			Snake snk = new Snake(new Point(4, 5, '*'), 9, Direction.Right);
-			snk.Draw();
-
 			FoodCreator foodCreator = new FoodCreator('@');
 			Point food = foodCreator.CreateFood();
+
+			snk.Draw();
+			walls.Draw();
 			food.Draw();
 
 			while (true)
 			{
 				if (walls.IsHit(snk) || snk.isHitTail())
 				{
-					PrintGameOver();
-					ConsoleKeyInfo key = Console.ReadKey();
-					while (true)
-					{
-						switch (key.KeyChar)
-						{
-							case 'Y':
-								{
-									Console.Clear();
-									StartGame();
-									break;
-								}
-							case 'y':
-								{
-									goto case 'Y';
-								}
-							default:
-								{
-									break;
-								}
-						}
-						break;
-					}
+					InitGameOver();
 					break;
 				}
 				if (snk.Eat(food))
 				{
-					foodEaten++;
 					food = foodCreator.CreateFood();
 					food.Draw();
-					Thread.Sleep(10);
-					Console.SetCursorPosition(14, 1);
-					Console.Write(foodEaten);
-					
 				}
-				Thread.Sleep(50);
 				if (Console.KeyAvailable)
 				{
-					ConsoleKeyInfo key = Console.ReadKey();
-					snk.SetMovementDirection(key);
+					snk.SetMovementDirection(Console.ReadKey());
 				}
 				Thread.Sleep(50);
 				snk.Move();
 			}
 		}
 
+		static void InitGameOver()
+		{
+			PrintGameOver();
+			ConsoleKeyInfo key = Console.ReadKey();
+			while (true)
+			{
+				switch (key.KeyChar)
+				{
+					case 'Y':
+						{
+							Console.Clear();
+							StartGame();
+							break;
+						}
+					case 'y':
+						{
+							goto case 'Y';
+						}
+					default:
+						{
+							break;
+						}
+				}
+				break;
+			}
+		}
 		static void PrintGameOver()
 		{
 			Console.Clear();
