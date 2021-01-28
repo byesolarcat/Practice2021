4ebbf711a689b3d77446002d79e9f3edf32e1ec9
byesolarcat <patachyon@gmail.com>
2021-01-28 12:04:37 +0300

    Add game initialization

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Snake
 {
@@ -6,6 +7,48 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
+			InitGameField();
+
+			StartGame();
+		}
+
+		static void InitGameField()
+		{
+			Console.SetWindowSize(121, 30);
+
+			HorizontalLine topBorder =
+				new HorizontalLine(Console.WindowLeft, Console.WindowWidth - 1, Console.WindowTop, '—');
+			HorizontalLine bottomBorder =
+				new HorizontalLine(Console.WindowLeft, Console.WindowWidth - 1, Console.WindowHeight - 1, '—');
+			HorizontalLine scoreBoardBorder =
+				new HorizontalLine(Console.WindowLeft, Console.WindowWidth - 1, Console.WindowTop + 2, '—');
+			VerticalLine leftBorder =
+				new VerticalLine(Console.WindowTop, Console.WindowHeight - 1, Console.WindowLeft, '|');
+			VerticalLine rightBorder =
+				new VerticalLine(Console.WindowTop, Console.WindowHeight - 1, Console.WindowWidth - 1, '|');
+
+			topBorder.Draw();
+			bottomBorder.Draw();
+			scoreBoardBorder.Draw();
+			leftBorder.Draw();
+			rightBorder.Draw();
+		}
+
+		static void StartGame()
+		{
+			Snake snk = new Snake(new Point(4, 5, '*'), 4, Direction.Right);
+			snk.Draw();
+
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snk.SetMovementDirection(key);
+				}
+				Thread.Sleep(64);
+				snk.Move();
+			}
 		}
 	}
 }
