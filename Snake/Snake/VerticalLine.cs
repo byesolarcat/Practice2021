7c6c298ecb322f16cdf15a8965e47656609aa14e
byesolarcat <patachyon@gmail.com>
2021-01-28 10:32:16 +0300

    Add "VerticalLine" class

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,27 @@
+﻿=using System.Collections.Generic;
+
+namespace Snake
+{
+	class VerticalLine
+	{
+		public List<Point> PointsList { get; set; }
+
+		public VerticalLine(int yTop, int yBottom, int x, char symbol)
+		{
+			PointsList = new List<Point>();
+			for (int y = yTop; y <= yBottom; y++)
+			{
+				Point p = new Point(x, y, symbol);
+				PointsList.Add(p);
+			}
+		}
+
+		public void Draw()
+		{
+			foreach (var point in PointsList)
+			{
+				point.Draw();
+			}
+		}
+	}
+}
